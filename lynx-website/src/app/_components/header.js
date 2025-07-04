fd203b23890ef72aff43e712075f91eef2a6152f
Murtaza Haque <murtaza0713@gmail.com>
2025-07-03 23:54:16 -0500

    Added Navbar and Started Making Changes to Header

--- lynx-website/src/app/_components/header.js
+++ lynx-website/src/app/_components/header.js
@@ -2,10 +2,16 @@ import styles from "./header.module.css"
 
 export default function Header() {
     return(
-        <header className={styles.page}>
-            <div className={styles.div}>
-                <p className={styles.divText}>My Website!</p>
-            </div>
+        <header className={styles.header}>
+            <img src="/public/ChooChoo.png" alt="Lynx Logo" className={styles.logo} />
+            
+            <nav className={styles.navbar}>
+                <a className={styles.divText}> Home </a>
+                <p className={styles.divText}>|</p>
+                <a className={styles.divText}> About </a>
+                <p className={styles.divText}>|</p>
+                <a className={styles.divText}> Contact </a>
+            </nav>
         </header>
     );
 }
