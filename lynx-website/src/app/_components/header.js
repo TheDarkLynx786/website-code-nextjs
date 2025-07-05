ca6a06ea4c8a01d6e2bf846ace5aeac99c7d31fe
Murtaza Haque <murtaza0713@gmail.com>
2025-07-04 23:22:32 -0500

    Added Links to Navbar Menu and Image Element

--- lynx-website/src/app/_components/header.js
+++ lynx-website/src/app/_components/header.js
@@ -3,14 +3,13 @@ import styles from "./header.module.css"
 export default function Header() {
     return(
         <header className={styles.header}>
-            <img src="/public/ChooChoo.png" alt="Lynx Logo" className={styles.logo} />
-            
+            <a href="/" > <img src="/public/ChooChoo.png" alt="Lynx Logo" className={styles.logo} /> </a>
             <nav className={styles.navbar}>
-                <a className={styles.divText}> Home </a>
+                <a href='/' className={styles.divText}> Home </a>
                 <p className={styles.divText}>|</p>
-                <a className={styles.divText}> About </a>
+                <a href='/about' className={styles.divText}> About </a>
                 <p className={styles.divText}>|</p>
-                <a className={styles.divText}> Contact </a>
+                <a href='/' className={styles.divText}> Contact </a>
             </nav>
         </header>
     );
