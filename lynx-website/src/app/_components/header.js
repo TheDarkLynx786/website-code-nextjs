82752bd8b51fb8cd356d99635d572b0bd6c79c9e
Murtaza Haque <murtaza0713@gmail.com>
2025-07-05 08:48:27 -0500

    Header Style Redesign and Changes

--- lynx-website/src/app/_components/header.js
+++ lynx-website/src/app/_components/header.js
@@ -1,15 +1,22 @@
 import styles from "./header.module.css"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
     return(
         <header className={styles.header}>
-            <a href="/" > <img src="/public/ChooChoo.png" alt="Lynx Logo" className={styles.logo} /> </a>
+            <Image
+                src="/images/ChooChoo.png"
+                alt="Site Logo"
+                width={50}
+                height={50} 
+                className={styles.logo}
+            />
+            
             <nav className={styles.navbar}>
-                <a href='/' className={styles.divText}> Home </a>
-                <p className={styles.divText}>|</p>
-                <a href='/about' className={styles.divText}> About </a>
-                <p className={styles.divText}>|</p>
-                <a href='/' className={styles.divText}> Contact </a>
+                <Link href='/' className={styles.divText}> Home </Link>
+                <Link href='/about' className={styles.divText}>About</Link>
+                <Link href='/' className={styles.divText}> Contact </Link>
             </nav>
         </header>
     );
