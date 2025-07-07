a54eb589bfda5f235a4182818c3fc7ee0ebd59f9
Murtaza Haque <murtaza0713@gmail.com>
2025-07-06 19:48:03 -0500

    Added Slide Down Effect For Header

--- lynx-website/src/app/_components/header.js
+++ lynx-website/src/app/_components/header.js
@@ -1,23 +1,62 @@
+"use client";
+
 import styles from "./header.module.css"
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+    const [scrollPos, setScrollPos] = useState(false);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            const triggerOffset = 300; // Show fixed header after scrolling this many px
+            setScrollPos(window.scrollY > triggerOffset);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    //84 Scroll Pos
+
     return(
+        <>
         <header className={styles.header}>
-            <Image
-                src="/images/ChooChoo.png"
-                alt="Site Logo"
-                width={50}
-                height={50} 
-                className={styles.logo}
-            />
-            
+            <Link href="/">
+                <Image
+                    src="/images/ChooChoo.png"
+                    alt="Site Logo"
+                    width={50}
+                    height={50} 
+                    className={styles.logo}
+                />
+            </Link>
+
             <nav className={styles.navbar}>
                 <Link href='/' className={styles.divText}> Home </Link>
                 <Link href='/about' className={styles.divText}>About</Link>
                 <Link href='/' className={styles.divText}> Contact </Link>
             </nav>
         </header>
+
+        <header className={`${styles.headerFixed} ${scrollPos ? styles.visible : ""}`}>
+            <Link href="/">
+                <Image
+                    src="/images/ChooChoo.png"
+                    alt="Site Logo"
+                    width={50}
+                    height={50} 
+                    className={styles.logo}
+                />
+            </Link>
+
+            <nav className={styles.navbar}>
+                <Link href='/' className={styles.divText}> Home </Link>
+                <Link href='/about' className={styles.divText}>About</Link>
+                <Link href='/' className={styles.divText}> Contact </Link>
+            </nav>
+        </header>
+        </>
     );
 }
