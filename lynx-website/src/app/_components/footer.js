1118ee823f49ddf82693ed6860c6f8309e17e420
Murtaza Haque <murtaza0713@gmail.com>
2025-07-08 06:08:41 -0500

    Footer Updates, Updated Favicon, Updated Folder Structure

--- lynx-website/src/app/_components/footer.js
+++ lynx-website/src/app/_components/footer.js
@@ -1,9 +1,38 @@
 import styles from "./footer.module.css"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+    
     return(
         <footer className={styles.footer}>
-            <p className={styles.divText}>© Murtaza Haque 2025</p>
+            <div className={styles.div}>
+                <Link href="https://github.com/TheDarkLynx786/" target="_blank" rel="noopener noreferrer">
+                        <Image
+                            src="/icons/brand-github.svg"
+                            alt="Site Logo"
+                            width={50}
+                            height={50} 
+                            className={styles.socialIcon}
+                        />
+                </Link>
+                <Link href="https://www.instagram.com/murtazahaque/" target="_blank" rel="noopener noreferrer">
+                        <Image
+                            src="/icons/brand-instagram.svg"
+                            alt="Site Logo"
+                            width={50}
+                            height={50} 
+                            className={styles.socialIcon}
+                        />
+                </Link>
+            </div>
+            
+            <div className={styles.div}>
+                <p className={styles.divText}>© {year} Copyright:&nbsp;</p>
+                <Link className={`${styles.divText} ${styles.link}`} href="/"> Murtaza Haque</Link>
+            </div>
         </footer>
     );
 }
