4a80f74bcf0e1e3310e0d5d41aecc31661d539d0
Murtaza Haque <murtaza0713@gmail.com>
2025-10-08 02:32:42 -0400

    Joined the Purdue Hackers Webring (+ got Linked-In)

--- lynx-website/src/_components/footer.js
+++ lynx-website/src/_components/footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
     
     return(
         <footer className={styles.footer}>
-            <div className={styles.div}>
+            <div className={styles.socialDiv}>
                 <Link href="https://github.com/TheDarkLynx786/" target="_blank" rel="noopener noreferrer">
                         <Image
                             src="/icons/brand-github.svg"
@@ -27,12 +27,54 @@ export default function Footer() {
                             className={styles.socialIcon}
                         />
                 </Link>
+                <Link href="https://www.linkedin.com/in/murtaza-haque" target="_blank" rel="noopener noreferrer">
+                        <Image
+                            src="/icons/brand-linkedin.svg"
+                            alt="Site Logo"
+                            width={50}
+                            height={50} 
+                            className={styles.socialIcon}
+                        />
+                </Link>
             </div>
             
-            <div className={styles.div}>
+            <div className={styles.webringDiv}>
+                
+
+                <div className={styles.webringButtonDiv}>
+                    <a className={styles.webringText} href="https://ring.purduehackers.com/previous">
+                        &lt;&lt;
+                    </a>
+                    Previous
+                </div>
+                
+                <div className={styles.webringButtonDiv}>
+                    <a href="https://ring.purduehackers.com/">
+                        <Image
+                                src="/images/PurdueHackers.png"
+                                alt="Purdue Hackers"
+                                width={50}
+                                height={50} 
+                                className={styles.webringLogo}
+                        />
+                    </a>
+                    Purdue Hackers Webring
+                </div>
+
+                <div className={styles.webringButtonDiv}>
+                    <a className={styles.webringText} href="https://ring.purduehackers.com/next">
+                        &gt;&gt;
+                    </a>
+                    Next
+                </div>
+            </div>
+
+            <div className={styles.textDiv}>
                 <p className={styles.divText}>© {year} Copyright:&nbsp;</p>
                 <Link className={`${styles.divText} ${styles.link}`} href="/"> Murtaza Haque</Link>
             </div>
+
+            
         </footer>
     );
 }
